57c49f9d2f8cc66c913d38b8373738078ae7011b
Thejaa Shree <160198737+thejathangavel@users.noreply.github.com>
2025-02-21 19:03:23 +0530

    Time: 0 ms (100%), Space: 46.2 MB (29.45%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,56 +1,15 @@
-/*public class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int[] result = new int[2];
-        result[0] = findLeft(nums, target);
-        result[1] = findRight(nums, target);
-        if (result[0] <= result[1]) {
-            return result;
-        } else {
-            return new int[] {-1, -1};
-        }
-    }
-    private int findLeft(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] >= target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-        return left;
-    }
-    private int findRight(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] <= target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return right;
-    }
-}
-*/
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int first = findPosition(nums, target, true);
         int last = findPosition(nums, target, false);
-        
         return new int[] { first, last };
     }
-
     private int findPosition(int[] nums, int target, boolean findFirst) {
         int left = 0;
         int right = nums.length - 1;
         int pos = -1;
-
         while (left <= right) {
             int mid = left + (right - left) / 2;
-
             if (nums[mid] == target) {
                 pos = mid;
                 if (findFirst) 
@@ -63,7 +22,6 @@ class Solution {
             else 
                 right = mid - 1;
         }
-
         return pos;
     }
 }
