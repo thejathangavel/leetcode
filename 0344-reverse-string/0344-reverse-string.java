f959d88f8beb6d6821eabab386520948deb62783
Thejaa Shree <160198737+thejathangavel@users.noreply.github.com>
2025-02-25 18:53:30 +0530

    Time: 124 ms (4.05%), Space: 46.3 MB (77.54%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void reverseString(char[] s) {
+      for(int start=0,end=s.length-1;start<=end;start++,end--){
+        char temp=s[start];
+        s[start]=s[end];
+        s[end]=temp;
+      }
+      for(char n:s){
+        System.out.print(n);
+      }
+      System.out.println();
+    }
+}
